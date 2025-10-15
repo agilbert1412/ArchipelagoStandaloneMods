ea2efd5287e245e34fd98a81bc774e4235a08319
Alex Gilbert <alexgilbert@yahoo.com>
2025-10-14 20:44:18 -0400

    - Initial Release for CustomMail

---
+++ CustomMail/CustomMail/CustomMail/CustomMailManager.cs
@@ -0,0 +1,68 @@
+﻿using HarmonyLib;
+using StardewModdingAPI;
+using StardewValley;
+
+namespace CustomMail.CustomMail
+{
+    public class CustomMailManager
+    {
+        private const string CUSTOM_MAIL_KEY_PREFIX = "CustomMail_";
+        private static IMonitor _logger;
+        private static IModHelper _modHelper;
+
+        public CustomMailManager(IMonitor logger, IModHelper modHelper)
+        {
+            _logger = logger;
+            _modHelper = modHelper;
+        }
+
+        public void AddLetterToday(string letterContent, bool topOfMailbox = false)
+        {
+            var mailKey = GenerateLetterData(letterContent);
+            if (topOfMailbox)
+            {
+                Game1.player.mailbox.Insert(0, mailKey);
+                _logger.Log($"Added letter '{mailKey}' to top of mailbox", LogLevel.Info);
+            }
+            else
+            {
+                Game1.player.mailbox.Add(mailKey);
+                _logger.Log($"Added letter '{mailKey}' to bottom of mailbox", LogLevel.Info);
+            }
+        }
+
+        public void AddLetterTomorrow(string letterContent)
+        {
+            var mailKey = GenerateLetterData(letterContent);
+            Game1.player.mailForTomorrow.Add(mailKey);
+            _logger.Log($"Added letter '{mailKey}' to tomorrow's mail", LogLevel.Info);
+        }
+
+        private static string GenerateLetterData(string letterContent)
+        {
+            var mailData = DataLoader.Mail(Game1.content);
+            var uniqueId = Guid.NewGuid().ToString();
+            var mailKey = $"{CUSTOM_MAIL_KEY_PREFIX}{uniqueId}";
+            mailData.Add(mailKey, letterContent);
+            _logger.Log($"Generated a new letter: '{mailKey}' -> '{letterContent}'", LogLevel.Info);
+            return mailKey;
+        }
+
+        public void GeneratePlaceholderContentForUnknownLetters()
+        {
+            const string placeholderTextTemplate = "This letter was added to the mailbox in a previous session and it doesn't have any content registered in the game data.";
+
+            var mailData = DataLoader.Mail(Game1.content);
+            foreach (var mailKey in Game1.player.mailbox.Union(Game1.player.mailForTomorrow))
+            {
+                if (mailData.ContainsKey(mailKey))
+                {
+                    continue;
+                }
+
+                mailData.Add(mailKey, placeholderTextTemplate);
+                _logger.Log($"Generated a default letter for a missing id: {mailKey}", LogLevel.Debug);
+            }
+        }
+    }
+}
---
+++ CustomMail/CustomMail/ModEntry.cs
@@ -0,0 +1,59 @@
+﻿using CustomMail.CustomMail;
+using HarmonyLib;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+
+namespace CustomMail
+{
+    internal sealed class ModEntry : Mod
+    {
+        public static ModEntry Instance;
+        private IModHelper _helper;
+        private CustomMailManager _customMailManager;
+
+        public ModEntry() : base()
+        {
+            Instance = this;
+        }
+
+        /*********
+         ** Public methods
+         *********/
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
+        public override void Entry(IModHelper helper)
+        {
+            _helper = helper;
+            _customMailManager = new CustomMailManager(Monitor, _helper);
+
+            _helper.Events.GameLoop.DayStarted += OnDayStarted;
+
+            _helper.ConsoleCommands.Add("add_letter", $"Adds a letter to the bottom of the mailbox. Syntax: add_letter [letter text]", OnAddLetter);
+            _helper.ConsoleCommands.Add("add_letter_top", $"Adds a letter to the top of the mailbox. Syntax: add_letter_top [letter text]", OnAddLetterTop);
+            _helper.ConsoleCommands.Add("add_letter_tomorrow", $"Adds a letter to the mail for tomorrow. Syntax: add_letter_tomorrow [letter text]", OnAddLetterTomorrow);
+        }
+
+        private void OnAddLetter(string arg1, string[] arg2)
+        {
+            var content = string.Join(" ", arg2);
+            _customMailManager.AddLetterToday(content, false);
+        }
+
+        private void OnAddLetterTop(string arg1, string[] arg2)
+        {
+            var content = string.Join(" ", arg2);
+            _customMailManager.AddLetterToday(content, true);
+        }
+
+        private void OnAddLetterTomorrow(string arg1, string[] arg2)
+        {
+            var content = string.Join(" ", arg2);
+            _customMailManager.AddLetterTomorrow(content);
+        }
+
+        private void OnDayStarted(object sender, DayStartedEventArgs e)
+        {
+            _customMailManager.GeneratePlaceholderContentForUnknownLetters();
+        }
+    }
+}
